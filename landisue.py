acdfc831272c479de4c898bb96ed10f65940ed19
lethargilistic <mikeoverby@outlook.com>
2016-05-31 23:58:43 -0700

    Boilerplate works

--- landisue.py
+++ landisue.py
@@ -1,9 +1,10 @@
+import keys
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
 
-api = tweepy.API(suth)
+api = tweepy.API(auth)
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
